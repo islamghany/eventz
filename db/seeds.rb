e844deb09c130ad203f7b515a6af992807d41c67
Islam Mostafa <islamghany3@gmail.com>
2024-06-11 02:20:29 +0300

    added tailwind css

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Event.create!([
+  { name: "Kata Camp", location: "Remote", price: 0, starts_at: "2024-06-10 12:00:00", description: "A day of kata practice" },
+  { name: "Code & Coffee", location: "Remote", price: 10.0, starts_at: "2024-06-11 08:00:00", description: "Start your day with code and coffee" },
+  { name: "RailsConf", location: "Portland, OR", price: 100, starts_at: "2024-06-12 00:00:00", description: "The largest official gathering of Ruby on Rails developers" },
+  { name: "Ruby Hack Night", location: "Remote", price: 0, starts_at: "2024-06-13 18:00:00", description: "Hacking on Ruby projects" },
+  { name: "Ruby Users Group", location: "Remote", price: 20.0, starts_at: "2024-06-14 18:00:00", description: %{
+    Monthly gathering of Ruby enthusiasts.
+  }.squish },
+])
