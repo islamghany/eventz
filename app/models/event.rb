7fb37655bd2b79c554d4c530b3288797859b7393
Islam Mostafa <islamghany3@gmail.com>
2024-06-11 11:10:14 +0300

    added custom queries to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,24 @@ class Event < ApplicationRecord
   def free?
     price.blank? || price.zero?
   end
+
+  def self.upcoming
+    where("starts_at > ?", Time.now).order("starts_at")
+  end
+
+  def self.past
+    where("starts_at < ?", Time.now).order("starts_at")
+  end
+
+  def self.by_price
+    order("price desc")
+  end
+
+  def self.by_name
+    order("name")
+  end
+
+  def self.search(term)
+    where("name ILIKE ? OR location ILIKE ?", "%#{term}%", "%#{term}%").order("starts_at")
+  end
 end
